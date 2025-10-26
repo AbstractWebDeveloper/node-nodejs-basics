b190210a4e3d77205088a8a8cffa8c42878e328c
Anthony <119855949+AbstractWebDeveloper@users.noreply.github.com>
2025-10-26 22:14:09 +0300

    Добавлена реализация функции parseEnv

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
-const parseEnv = () => {
-  // Write your code here
-};
 
-parseEnv();
+const PREFIX = 'RSS_'
+
+const parseEnv = (prefix) => {
+    const args = Object.entries(process.env)
+          .filter(([key, _]) => key.startsWith(prefix))
+          .map(([key, value]) => `${key}=${value}`)
+          .join('; ')
+
+    console.log(args)
+}
+
+parseEnv(PREFIX)
