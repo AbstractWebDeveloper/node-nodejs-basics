2446c230ba51b01c6dc7b8a9ea42dddcce43c3f1
Anthony <119855949+AbstractWebDeveloper@users.noreply.github.com>
2025-10-26 21:27:26 +0300

    Добавлена реализация функции create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
-const create = async () => {
-  // Write your code here
+import { writeFile } from "fs/promises"
+
+const FILE_PATH = "src/fs/files/fresh.txt"
+const DATA = "I am fresh and young"
+const ERROR_MSG = "Create operation failed"
+
+const create = async (path, data, flag = 'wx') => {
+    try {
+        await writeFile(path, data, {flag}) 
+    } catch (error) {
+        throw new Error(ERROR_MSG, {cause: error})
+    }
+
 };
 
-await create();
+await create(FILE_PATH, DATA)
