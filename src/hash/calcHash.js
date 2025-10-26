f7ef176f5a4a745fbce833c4d2cd4ab322b4dfa5
Anthony <119855949+AbstractWebDeveloper@users.noreply.github.com>
2025-10-26 23:24:22 +0300

    Добавлена реализация функции calculateHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
-const calculateHash = async () => {
-  // Write your code here
+import { createHash } from 'crypto'
+import { createReadStream } from "fs"
+import { pipeline } from 'stream/promises';
+
+
+const HASH_FILE_PATH = "src/hash/files/fileToCalculateHashFor.txt"
+const DEFAULT_HASH_TYPE = "sha256"
+const ERROR_MSG = "Calculate hash failed"
+
+const calculateHash = async (path, hashType = DEFAULT_HASH_TYPE) => {
+  try {
+    const hash = createHash(hashType)
+    const stream = createReadStream(path)
+
+    await pipeline(stream, hash);
+
+    console.log(hash.digest('hex'))
+  } catch (error) {
+     throw new Error(ERROR_MSG, { cause: error })
+  }
 };
 
-await calculateHash();
+await calculateHash(HASH_FILE_PATH)
