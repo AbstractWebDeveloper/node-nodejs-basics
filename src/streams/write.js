619007d94d45f776c4f44ed25e4e7eaacead17f7
Anthony <119855949+AbstractWebDeveloper@users.noreply.github.com>
2025-10-27 00:47:20 +0300

    Добавлена реализация функции write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
-const write = async () => {
-  // Write your code here
+import { createWriteStream } from 'fs'
+import { stdin } from "process"
+import { pipeline } from "stream/promises"
+
+const WRITE_FILE_PATH = "src/streams/files/fileToWrite.txt"
+const ERROR_MSG = "Write stream failed"
+
+const write = async (path, options = { flags: 'w+' }) => {
+  try {
+    const writeStream = createWriteStream(path, options)
+    await pipeline(stdin, writeStream)
+  } catch (error) {
+    throw new Error(ERROR_MSG, {cause: error})
+  }
+
 };
 
-await write();
+await write(WRITE_FILE_PATH)
