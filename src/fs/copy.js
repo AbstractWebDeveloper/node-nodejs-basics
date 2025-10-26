0943b09b44a7999cde1c340050cfbff253219eb6
Anthony <119855949+AbstractWebDeveloper@users.noreply.github.com>
2025-10-26 21:27:35 +0300

    Добавлена реализация функции copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,15 @@
-const copy = async () => {
-  // Write your code here
+import { cp, constants  } from "fs/promises"
+
+const ORIGINAL_FOLDER_PATH = "src/fs/files"
+const COPY_FOLDER_PATH = "src/fs/files_copy"
+const ERROR_MSG = "Copy operation failed"
+
+const copy = async (src, dets, options = {recursive : true, errorOnExist: true, force: false, mode: constants.COPYFILE_EXCL}) => {
+    try {
+      cp(src, dets, options)
+    } catch (error) {
+        throw new Error(ERROR_MSG, {cause: error})
+    }
 };
 
-await copy();
+await copy(ORIGINAL_FOLDER_PATH, COPY_FOLDER_PATH)
