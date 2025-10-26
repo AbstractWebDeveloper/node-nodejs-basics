8144c1a2df62a05a58453560e25f0c97a4752195
Anthony <119855949+AbstractWebDeveloper@users.noreply.github.com>
2025-10-27 01:04:17 +0300

    Добавлена реализация функций compress и decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
-const compress = async () => {
-  // Write your code here
-};
+import { createReadStream, createWriteStream } from "fs"
+import { pipeline } from "stream/promises"
+import { createGzip } from 'zlib'
 
-await compress();
+const COMPRESS_FILE_PATH = "src/zip/files/fileToCompress.txt"
+const ZIP_FILE_PATH = "src/zip/files/archive.gz"
+const ERROR_MSG = "Compress stream failed"
+
+const compress = async (path, dstPath) => {
+  try {
+    await pipeline(createReadStream(path), createGzip(), createWriteStream(dstPath))
+  } catch (error) {
+    throw new Error(ERROR_MSG, {cause: error})
+  }
+}
+
+await compress(COMPRESS_FILE_PATH, ZIP_FILE_PATH)
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
-const decompress = async () => {
-  // Write your code here
-};
+import { createReadStream, createWriteStream } from "fs"
+import { pipeline } from "stream/promises"
+import { createGunzip } from 'zlib'
 
-await decompress();
+const ZIP_FILE_PATH = "src/zip/files/archive.gz"
+const DECOMPRESS_FILE_PATH = "src/zip/files/fileToCompress.txt"
+const ERROR_MSG = "Decompress stream failed"
+
+
+const decompress = async (path, dstPath) => {
+  try {
+    await pipeline(createReadStream(path), createGunzip(), createWriteStream(dstPath))
+  } catch (error) {
+    throw new Error(ERROR_MSG, { cause: error })
+  }
+}
+
+await decompress(ZIP_FILE_PATH, DECOMPRESS_FILE_PATH)
