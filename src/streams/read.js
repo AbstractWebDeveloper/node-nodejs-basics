7f8209383208af7ee76ba413e6ac4108dc15fb1f
Anthony <119855949+AbstractWebDeveloper@users.noreply.github.com>
2025-10-27 00:38:53 +0300

    Добавлена реализация функции read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
-const read = async () => {
-  // Write your code here
+import { createReadStream } from 'fs'
+import { stdout } from 'process'
+import { pipeline } from 'stream/promises'
+import { EOL } from 'os'
+
+const READ_FILE_PATH = 'src/streams/files/fileToRead.txt'
+const ERROR_MSG = 'Read stream failed'
+
+const read = async (path) => {
+  try {
+    await pipeline(createReadStream(path), stdout, { end: false })
+    stdout.write(EOL)
+    stdout.end()
+  } catch (error) {
+    throw new Error(ERROR_MSG, { cause: error })
+  }
 };
 
-await read();
+await read(READ_FILE_PATH)
