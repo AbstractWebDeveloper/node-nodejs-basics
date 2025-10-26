b87069120c15fd9468e1397927414f6eef4ba76e
Anthony <119855949+AbstractWebDeveloper@users.noreply.github.com>
2025-10-27 01:07:11 +0300

    Исправлена функция copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -4,11 +4,11 @@ const ORIGINAL_FOLDER_PATH = "src/fs/files"
 const COPY_FOLDER_PATH = "src/fs/files_copy"
 const ERROR_MSG = "Copy operation failed"
 
-const copy = async (src, dets, options = {recursive : true, errorOnExist: true, force: false, mode: constants.COPYFILE_EXCL}) => {
+const copy = async (src, dest, options = {recursive : true, errorOnExist: true, force: false, mode: constants.COPYFILE_EXCL}) => {
     try {
-      cp(src, dets, options)
+      await cp(src, dest, options)
     } catch (error) {
-        throw new Error(ERROR_MSG, {cause: error})
+      throw new Error(ERROR_MSG, {cause: error})
     }
 };
 
