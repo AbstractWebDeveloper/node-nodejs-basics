da66b6f21f6c105baf845fae2f320b2eb2691ce6
Anthony <119855949+AbstractWebDeveloper@users.noreply.github.com>
2025-10-26 22:02:14 +0300

    Добавлена реализация функции list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
-const list = async () => {
-  // Write your code here
-};
+import { readdir } from "fs/promises"
 
-await list();
+const READ_FOLDER_PATH = "src/fs/files"
+const ERROR_MSG = "List operation failed"
+
+const list = async (folder, options = { recursive: true }) => {
+    try {
+        console.log(await readdir(folder, options))
+    } catch (error) {
+        throw new Error(ERROR_MSG, {cause: error})
+    }
+}
+
+await list(READ_FOLDER_PATH)
