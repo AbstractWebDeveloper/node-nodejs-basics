422892c503d5691d3ac1ef9fdb6b0082ceb2c945
Anthony <119855949+AbstractWebDeveloper@users.noreply.github.com>
2025-10-26 21:56:19 +0300

    Добавлена реализация функции rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
-const rename = async () => {
-  // Write your code here
+import { copyFile, unlink, constants } from "fs/promises"
+import { dirname, join } from "path"
+
+const ORIGINAL_FILE_PATH = "src/fs/files/wrongFilename.txt"
+const NEW_FILENAME = "properFilename.md"
+const ERROR_MSG = "Rename operation failed"
+
+const rename = async (path, newName, mode = constants.COPYFILE_EXCL) => {
+  try {
+      const destFilePath = join(dirname(path), newName);
+      await copyFile(path, destFilePath, mode);
+      await unlink(path)
+
+    } catch (error) {
+      throw new Error(ERROR_MSG, { cause: error })
+    }
 };
 
-await rename();
+await rename(ORIGINAL_FILE_PATH, NEW_FILENAME)
