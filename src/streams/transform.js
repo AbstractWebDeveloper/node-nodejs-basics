f369f542f8e845a44d112274f4bef34d11e2cf83
Anthony <119855949+AbstractWebDeveloper@users.noreply.github.com>
2025-10-27 00:52:51 +0300

    Добавлена реализация функции transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
-const transform = async () => {
-  // Write your code here
-};
+import { stdin, stdout } from 'process'
+import { Transform, } from 'stream'
+import { pipeline } from "stream/promises"
+import { EOL } from 'os'
 
-await transform();
+const reverseStream = new Transform({
+    transform(chunk, _, callback) {
+        const reversed = chunk.toString().split('').reverse().join('') + EOL
+        callback(null, reversed)
+    }
+})
+
+const transform = async () => {
+    await pipeline(stdin, reverseStream, stdout)
+}
+
+await transform()
