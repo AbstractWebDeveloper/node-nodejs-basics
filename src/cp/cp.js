a6540593447e9eb3f59d4f20c2f9367f7b6685b9
Anthony <119855949+AbstractWebDeveloper@users.noreply.github.com>
2025-10-27 17:54:55 +0300

    Исправлен cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const SCRIPT_PATH = join(__dirname, "files",'script.js')
 
-const spawnChildProcess = async (args) => {
+const spawnChildProcess = async (...args) => {
     const child = fork(SCRIPT_PATH, args, { silent: true })
     stdin.pipe(child.stdin)
     child.stdout.pipe(stdout)
